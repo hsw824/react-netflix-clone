4f050f10c3e33cbbaaf51ae67e6c7da702d6d902
hsw824 <ghdtjddn0824@naver.com>
2022-08-29 21:14:50 +0900

    동영상 없을 경우 오류 해결

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -55,6 +55,7 @@ const Banner = () => {
             >
               Play
             </button>
+
             <button className="banner__button info">More information</button>
           </div>
 
@@ -74,7 +75,7 @@ const Banner = () => {
             <Iframe
               width="640"
               height="360"
-              src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+              src={`https://www.youtube.com/embed/${movie.videos.results[0]?.key}?controls=0&autoplay=1&loop=1&playlist=${movie.videos.results[0]?.key}`}
               frameborder="0"
               allow=" autoplay; fullscreen"
             ></Iframe>
